5de695cba75a62ae40f64541724988aa4f3cfcf5
Cze Wen Goh <czewengoh@Hotmail.com>
2025-09-09 03:45:10 -0700

    Update build config to use base url for deployment

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: 'https://czewen.github.io/hsr-char-guesser-deploy/',
   plugins: [react()],
 })
